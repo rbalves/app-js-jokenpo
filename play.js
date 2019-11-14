fb2350a621d13de3165079fc6b05b6a5ec242d6a
Rafael Alves <rbalves.ads@gmail.com>
2019-11-13 21:42:05 -0400

    General changes

--- play.js
+++ play.js
@@ -1,36 +1,33 @@
 var rounds = 5, round = 1;
 
 var player1 = {
-    name: "Jogador",
+    name: "Você",
     choice: "?",
     points: 0
 }
 
 var player2 = {
-    name: "Computador",
+    name: "Máquina",
     choice: "?",
     points: 0
 }
 
-var message;
-
-var dataDivs;
+var message, dataDivs;
 
 function setDivs() {
-    setarValuesDiv();
-
+    setValuesDiv();
     dataDivs.forEach(div => {
         clearDiv(div);
         mountDiv(div);
     });
 }
 
-function setarValuesDiv() {
+function setValuesDiv() {
     dataDivs = [
         {
             element: "h2",
             text: "Rodada " + round + " / " + rounds,
-            id: "div-rounds",
+            id: "rounds",
         },
         {
             element: "h4",
@@ -40,7 +37,7 @@ function setarValuesDiv() {
         {
             element: "h3",
             text: player1.name + " X " + player2.name,
-            id: "name-players",
+            id: "players",
         },
         {
             element: "h3",
@@ -56,8 +53,7 @@ function setarValuesDiv() {
 }
 
 function clearDiv({id}) {
-    let div = document.getElementById(id);
-    div.innerHTML = "";
+    document.getElementById(id).innerHTML = "";
 }
 
 function mountDiv({element, text, id}) {
@@ -67,22 +63,21 @@ function mountDiv({element, text, id}) {
     div.appendChild(elementText);
 }
 
-var divsButtonsPlay = ['button-stone', 'button-paper', 'button-scissors', 'button-new-play'];
+var idsAllButtons = ['button-stone', 'button-paper', 'button-scissors', 'button-new-play'];
 
 function clickButtonChoice(choice){
     player1.choice = choice;
     player2.choice = randomChoice();
-
-    divsButtonsPlay.forEach(button => {
-        document.getElementById(button).disabled = true;
-    });
-
+    disableButtons(true);
     checkWinnerRound();
-
     setDivs();
-
     setTimeout(nextRound, 2000);
+}
 
+function disableButtons(status) {
+    idsAllButtons.forEach(button => {
+        document.getElementById(button).disabled = status;
+    });
 }
 
 function randomChoice() {
@@ -93,38 +88,41 @@ function randomChoice() {
     return choices[numberRandom];
 }
 
+var won = "Você ganhou a rodada!";
+var defeat = "Você perdeu a rodada!";
+
 function checkWinnerRound() {
     if(player1.choice.includes(player2.choice)){
         player1.points++;
         player2.points++;
-        message = "Empatou!";
+        message = "Rodada empatada!";
     }else{
         switch (player1.choice) {
             case "Pedra":
                 if(player2.choice.includes("Tesoura")){
                     player1.points++;
-                    message = "Você ganhou a rodada!";
+                    message = won;
                 }else{
                     player2.points++;
-                    message = "Você perdeu a rodada!";
+                    message = defeat;
                 }
                 break;
             case "Papel":
                 if(player2.choice.includes("Pedra")){
                     player1.points++;
-                    message = "Você ganhou a rodada!";
+                    message = won;
                 }else{
                     player2.points++;
-                    message = "Você perdeu a rodada!";
+                    message = defeat;
                 }
                 break;
             default:
                 if(player2.choice.includes("Papel")){
                     player1.points++;
-                    message = "Você ganhou a rodada!";
+                    message = won;
                 }else{
                     player2.points++;
-                    message = "Você perdeu a rodada!";
+                    message = defeat;
                 }
                 break;
         }
@@ -133,22 +131,19 @@ function checkWinnerRound() {
 
 function nextRound() {
     round++;
-
     if (round <= rounds) {
         player1.choice = "?";
         player2.choice = "?";
         message = "Aguardando sua escolha...";
         setDivs();
-        divsButtonsPlay.forEach(button => {
-            document.getElementById(button).disabled = false;
-        });
+        disableButtons(false);
     }else{
         if(player1.points > player2.points){
             alert("Você ganhou a partida!");
         }else if(player1.points < player2.points){
             alert("Você perdeu a partida!");
         }else{
-            alert("Empatou!");
+            alert("Partida empatada!");
         }
         document.getElementById('button-new-play').disabled = false;
     }
@@ -156,21 +151,13 @@ function nextRound() {
 }
 
 function newPlay() {
-
     round = 1;
-
     player1.choice = "?";
     player1.points = 0;
-
     player2.choice = "?";
     player2.points = 0;
-
     message = "Aguardando sua escolha...";
-
-    divsButtonsPlay.forEach(button => {
-        document.getElementById(button).disabled = false;
-    });
-
+    disableButtons(false);
     setDivs();
 }
 
